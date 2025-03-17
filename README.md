2d8db32e8b6a83b22647376a9db88c58ca0554e5
polluxdev <fahminur.dev@gmail.com>
2025-03-17 16:08:13 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# go-serverx
+
+# go-serverx - Web Server Toolkit for Go
+
+`go-serverx` is a modular web server package for Go, supporting [net/http](https://pkg.go.dev/net/http) and [valyala/fasthttp](https://pkg.go.dev/github.com/valyala/fasthttp) package. It provides an easy and structured way to manage web server mechanism in your Go projects.
+
+## Features
+- 🔌 **Modular:** Separate packages for each web server.
+- 🛠️ **Reusable:** Designed for use in multiple projects.
+- 📖 **Easy to Use:** Simple configuration setup.
+
+## Installation
+```sh
+go get github.com/polluxdev/go-serverx
---
+++ fasthttp/fasthttp.go
@@ -0,0 +1,65 @@
+package fasthttp
+
+import (
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+const (
+	_defaultReadTimeout     = 5 * time.Second
+	_defaultWriteTimeout    = 5 * time.Second
+	_defaultAddr            = ":80"
+	_defaultShutdownTimeout = 3 * time.Second
+)
+
+// Server -.
+type Server struct {
+	server          *fasthttp.Server
+	notify          chan error
+	shutdownTimeout time.Duration
+	addr            string
+}
+
+// New -.
+func New(handler fasthttp.RequestHandler, opts ...Option) *Server {
+	httpServer := &fasthttp.Server{
+		Handler:      handler,
+		ReadTimeout:  _defaultReadTimeout,
+		WriteTimeout: _defaultWriteTimeout,
+	}
+
+	s := &Server{
+		server:          httpServer,
+		notify:          make(chan error, 1),
+		shutdownTimeout: _defaultShutdownTimeout,
+		addr:            _defaultAddr,
+	}
+
+	// Custom options
+	for _, opt := range opts {
+		opt(s)
+	}
+
+	s.start()
+
+	return s
+}
+
+// start -.
+func (s *Server) start() {
+	go func() {
+		s.notify <- s.server.ListenAndServe(s.addr)
+		close(s.notify)
+	}()
+}
+
+// Notify -.
+func (s *Server) Notify() <-chan error {
+	return s.notify
+}
+
+// Shutdown -.
+func (s *Server) Shutdown() error {
+	return s.server.Shutdown()
+}
